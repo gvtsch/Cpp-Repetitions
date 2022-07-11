0d069a65bf12bbcd1d946bf0eb55da3d0da63dd7
Christoph <88708549+gvtsch@users.noreply.github.com>
2022-07-11 17:34:24 +0200

    Create pointer_in_cpp.cpp

---
+++ pointer_in_cpp.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+void update(int *a,int *b) {
+    int sum = *a + *b;
+    int dif;
+    if (*a >= *b) {
+        dif = *a - *b;    
+    } else {
+    dif = *b - *a;
+    }
+    *a = sum;
+    *b = dif;
+
+    
+    // Complete this function    
+}
+
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+    
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+
+    return 0;
+}
