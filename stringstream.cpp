9a8252dec9654b66acff3341082cf14fcde00a1b
Christoph <88708549+gvtsch@users.noreply.github.com>
2022-07-11 17:35:46 +0200

    Create stringstream.cpp

---
+++ stringstream.cpp
@@ -0,0 +1,31 @@
+#include <sstream>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+vector<int> parseInts(string str) {
+    // Complete this function
+    vector<int> vec;        // Declares a vector to store the ints
+    stringstream ss(str);   // Declares a stringstream object to deal with the modification of the string
+    char ch;
+    int temp;
+    
+    while(ss)               // While the stringstream object does not hit a null byte
+    {
+        ss>>temp>>ch;           // Extract the comma seperated ints with the extraction >> operator
+        vec.push_back(temp);   // Push the int onto the vector
+    } 
+    
+    return vec; // Return the vector of ints
+}
+
+int main() {
+    string str; // a string of comma separated integers
+    cin >> str;
+    vector<int> integers = parseInts(str); // a vector of the parsed integers
+    for(int i = 0; i < integers.size(); i++) {
+        cout << integers[i] << "\n";
+    }
+    
+    return 0;
+}
